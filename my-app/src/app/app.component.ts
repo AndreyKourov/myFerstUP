77be46055ce6d451db1511ac0504b817f0db0fc7
Andrey <you@examp>
2020-03-24 20:21:17 +0700

    init

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+  desc = '1111111';
+  products = [
+    
+    {
+      title: 'Продукт 1',
+      desc: 'Описание 1',
+    },
+    {
+      title: 'Продукт 2',
+      desc: 'Описание 2',
+    },
+  ]
+
+}
